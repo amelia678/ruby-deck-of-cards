87157ba860d813da9f7532246d3f781851608d49
Amelia Schulz <amelia.schulz@gmail.com>
2019-02-12 16:11:57 -0500

    adds tty-prompt

--- poker.rb
+++ poker.rb
@@ -1,8 +1,13 @@
+
+require 'tty-prompt'
+
+prompt = TTY::Prompt.new
+
 suits = ['H', 'D', 'S', 'C']
 
 values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 
-puts suits
+# puts suits
 
 deck = []
 
@@ -12,6 +17,8 @@ suits.each do |suit|
     end
 end
 
-puts deck
-puts deck.length
+
+puts deck.shuffle.first(2)
+
+prompt.ask("Would you like to play cards")
 
