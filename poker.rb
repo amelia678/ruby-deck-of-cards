882e93e34132b0a2dca0d121a0958f21db72abed
Amelia Schulz <amelia.schulz@gmail.com>
2019-02-13 10:26:16 -0500

    Clean up white space and indentation

--- poker.rb
+++ poker.rb
@@ -4,10 +4,7 @@ require 'tty-prompt'
 prompt = TTY::Prompt.new
 
 suits = ['H', 'D', 'S', 'C']
-
 values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-
-
 deck = []
 
 suits.each do |suit|
@@ -18,14 +15,12 @@ end
 
 name = prompt.ask('What is your name?')
 
-
 is_cards = prompt.yes?("Would you like to play cards, #{name}?")
 if is_cards
     puts 'Lets play'
     puts 'shuffling deck...'
     puts 'take two cards'
     puts  deck.shuffle.first(2)
-    
 else
     puts 'Darn!' 
 end
